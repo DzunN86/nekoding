3f9c9b0c0262105b0d288acf8686c3b3c8ff57c7
DzunN86 <nurroinz@gmail.com>
2022-11-22 22:00:54 +0700

    ..init

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,18 +1,6 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-export const queryClient = new QueryClient();
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </ChakraProvider>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,49 +1,9 @@
-import { Flex, Text } from "@chakra-ui/react";
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import React from 'react';
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Nekoding</title>
-        <meta
-          name="description"
-          content="Upgrade your skill. Start here with us"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Flex
-        direction="column"
-        align="center"
-        justify="center"
-        height="100vh"
-      >
-        <Text
-          fontSize="8xl"
-          fontWeight="bold"
-          bgGradient="linear(to-l, #8AD4EC,#EF96FF,#FF56A9,#FFAA6C)"
-          bgClip="text"
-          textAlign="center"
-          mt="10"
-        >
-          Nekoding
-        </Text>
-        <Text
-          fontSize="4xl"
-          fontWeight="bold"
-          letterSpacing={15}
-          bgGradient="linear(to-l, #8AD4EC,#EF96FF,#FF56A9,#FFAA6C)"
-          bgClip="text"
-          textAlign="center"
-        >
-          Coming Soon!
-        </Text>
-      </Flex>
+    <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-tr from-[#8AD4EC] via-[#EF96FF] to-[#FFAA6C]">
+      <h1 className="text-6xl font-bold text-white">Nekoding</h1>
     </div>
   );
-};
-
-export default Home;
+}
