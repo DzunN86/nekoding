61da85ab4dca628a555d28cb23defb0bdd4bf25d
DzunN86 <nurroinz@gmail.com>
2022-11-28 00:03:03 +0700

    transition hover button

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="flex h-screen w-full items-center justify-center font-poppins">
+    <div className="relative flex h-screen w-full items-center justify-center font-poppins">
       <div className="flex w-full items-center justify-between px-[155px]">
         <div className="flex flex-col">
           <div className="mb-9 max-w-xl text-6xl font-bold text-white">
@@ -13,13 +13,13 @@ export default function Hero() {
             achieve it.
           </div>
           <div className="flex max-w-[23rem] justify-between">
-            <div className="flex h-12 w-40 cursor-pointer flex-col rounded-md bg-gradient-to-r from-[#8AD4EC] via-[#FF56A9] to-[#FFAA6C] transition-all duration-150 hover:bg-gradient-to-tr">
+            <div className="flex h-12 w-40 cursor-pointer flex-col rounded-md bg-gradient-to-r from-[#8AD4EC] via-[#FF56A9] to-[#FFAA6C] bg-size-200 bg-pos-0 transition-all duration-300 hover:bg-pos-100">
               <div className="my-auto items-center text-center text-xs font-semibold tracking-widest text-white">
                 REGISTER
               </div>
             </div>
-            <div className="flex h-12 w-40 cursor-pointer flex-col rounded-md border-2 border-transparent bg-gradient-to-tr from-[#8AD4EC] via-[#FF56A9] to-[#FFAA6C] bg-clip-border hover:border-none">
-              <div className="flex h-full w-full flex-col rounded-md bg-[#32394A] text-transparent transition-all duration-150 hover:bg-gradient-to-r hover:from-[#8AD4EC] hover:via-[#FF56A9] hover:to-[#FFAA6C] hover:text-white">
+            <div className="flex h-12 w-40 cursor-pointer flex-col rounded-md border-2 border-transparent bg-gradient-to-tr from-[#8AD4EC] via-[#FF56A9] to-[#FFAA6C] bg-clip-border transition-all duration-300 hover:border-none">
+              <div className="flex h-full w-full flex-col rounded-md bg-[#32394A] text-transparent transition-all duration-300 hover:bg-gradient-to-r hover:from-[#8AD4EC] hover:via-[#FF56A9] hover:to-[#FFAA6C] hover:text-white">
                 <div className="my-auto items-center bg-gradient-to-r from-[#8AD4EC] via-[#FF56A9] to-[#FFAA6C] bg-clip-text text-center text-xs font-semibold tracking-widest">
                   EXPLORE COURSE
                 </div>
